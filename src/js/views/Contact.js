957f43062071d99fe93dacbc8faf2bbc4695a2bb
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-20 17:33:54 +0000

    Add icons, Add query selector

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -15,14 +15,30 @@ export const Contact = () => (
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">
-                Name Last Name <i className="fas fa-pen-square"></i>
-              </h5>
-              <p className="card-text">5555 Road Street</p>
-              <p className="card-text">(506) 7777-7777</p>
-              <p className="card-text">
-                <small className="text-muted">test@mail.com</small>
-              </p>
+              <div className="card-edit-trash-icons-container">
+                <div>
+                  <span className="edit-delete-icons">
+                    <i className="fas fa-pen-square p-2"></i>
+                    <i class="fa fa-trash p-2"></i>
+                  </span>
+                </div>
+                <div>
+                  <h5 className="card-title">Name Last Name</h5>
+                  <p className="card-text">
+                    <i class="fas fa-map-marker m-1"></i>
+                    5555 Road Street
+                  </p>
+                  <p className="card-text">
+                    <i class="fa fa-phone m-1"></i>(506) 7777-7777
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      {' '}
+                      <i class="fa fa-envelope m-1"></i>test@mail.com
+                    </small>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -40,12 +56,30 @@ export const Contact = () => (
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">Name Last Name</h5>
-              <p className="card-text">5555 Road Street</p>
-              <p className="card-text">(506) 7777-7777</p>
-              <p className="card-text">
-                <small className="text-muted">test@mail.com</small>
-              </p>
+              <div className="card-edit-trash-icons-container">
+                <div>
+                  <span className="edit-delete-icons">
+                    <i className="fas fa-pen-square p-2"></i>
+                    <i class="fa fa-trash p-2"></i>
+                  </span>
+                </div>
+                <div>
+                  <h5 className="card-title">Name Last Name</h5>
+                  <p className="card-text">
+                    <i class="fas fa-map-marker m-1"></i>
+                    5555 Road Street
+                  </p>
+                  <p className="card-text">
+                    <i class="fa fa-phone m-1"></i>(506) 7777-7777
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      {' '}
+                      <i class="fa fa-envelope m-1"></i>test@mail.com
+                    </small>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
