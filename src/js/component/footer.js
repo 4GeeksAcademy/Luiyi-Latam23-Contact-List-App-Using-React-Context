57791b3f93f70c56a766557b5f1e540a70cef6ce
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-20 03:27:48 +0000

    nav-bar icon added, css set

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center">
+  <footer className="footer mt-3 py-3 text-center">
     <p>
       Contact-List App <strong>&copy; 2023</strong>{' '}
     </p>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light mb-3">
+    <nav className="navbar navbar-light mb-3 mt-3 p-3">
       <Link to="/">
-        <span className="navbar-brand text-white mb-0 h1">Contact List 📋</span>
+        <span className="navbar-brand text-white h1 custom-navbar-brand">
+          Contact List<i className="far fa-address-book"></i>
+        </span>
       </Link>
       <div className="ml-auto">
         <Link to="/demo">
