384c87ba84327b3bedf2597e1d742dbfc302e3ed
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-20 15:02:16 +0000

    Adjust cardImage, adjust card fields

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -4,25 +4,22 @@ import '../../styles/home.css';
 export const Contact = () => (
   <div>
     <div className="text-center mt-5">
-      <div className="card mb-3" style={{}}>
+      <div className="card mb-3">
         <div className="row g-0">
-          <div className="col-md-4">
+          <div className="col-md-4 d-flex justify-content-center align-items-center">
             <img
               src="https://images.pexels.com/photos/264905/pexels-photo-264905.jpeg?auto=compress&cs=tinysrgb&w=1200"
-              className="img-fluid rounded-start"
+              className="cardImage"
               alt="..."
             />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Name Last Name</h5>
+              <p className="card-text">5555 Road Street</p>
+              <p className="card-text">(506) 7777-7777</p>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">test@mail.com</small>
               </p>
             </div>
           </div>
@@ -30,25 +27,22 @@ export const Contact = () => (
       </div>
     </div>
     <div className="text-center mt-5">
-      <div className="card mb-3" style={{}}>
+      <div className="card mb-3">
         <div className="row g-0">
           <div className="col-md-4">
             <img
               src="https://images.pexels.com/photos/264905/pexels-photo-264905.jpeg?auto=compress&cs=tinysrgb&w=1200"
-              className="img-fluid rounded-start"
+              className="cardImage"
               alt="..."
             />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Name Last Name</h5>
+              <p className="card-text">5555 Road Street</p>
+              <p className="card-text">(506) 7777-7777</p>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">test@mail.com</small>
               </p>
             </div>
           </div>
