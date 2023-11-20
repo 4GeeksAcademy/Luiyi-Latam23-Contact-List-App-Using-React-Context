9c43175955ef26fec15810d6a63add0226815826
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-20 01:25:29 +0000

    Footer set up

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p>
+      Contact-List App <strong>&copy; 2023</strong>{' '}
+    </p>
+    <p>
+      Created with ❤️ by <strong>LUIYI</strong> 🤓{' '}
+    </p>
+  </footer>
 );
